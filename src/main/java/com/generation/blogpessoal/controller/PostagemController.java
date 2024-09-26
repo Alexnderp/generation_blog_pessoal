6025d598b1938bd8e1dbcd02ac60f697f03c482c
Alexander Pereira <xandi9731@gmail.com>
2024-09-26 10:42:55 -0300

    Feat: Implementação da rota de gerar post com IA

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,6 +3,7 @@ package com.generation.blogpessoal.controller;
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 import com.generation.blogpessoal.repository.TemaRepository;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -26,12 +27,14 @@ public class PostagemController {
     private PostagemRepository postagemRepository;
     @Autowired
     private TemaRepository temaRepository;
+    @Autowired
+    private ChatLanguageModel chatLanguageModel;
 
     @Operation(summary = "Get all posts",
             description = "This route returns all posts",
             tags = {"posts", "get"})
     @GetMapping
-    public ResponseEntity<List<Postagem>> getAll(){
+    public ResponseEntity<List<Postagem>> getAll() {
         return ResponseEntity.ok(postagemRepository.findAll());
     }
 
@@ -39,7 +42,7 @@ public class PostagemController {
             description = "This route returns a specific post based on the id provided",
             tags = {"posts", "get"})
     @GetMapping("/{id}")
-    public ResponseEntity<Postagem> getById(@PathVariable UUID id){
+    public ResponseEntity<Postagem> getById(@PathVariable UUID id) {
         return postagemRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
@@ -49,7 +52,7 @@ public class PostagemController {
             description = "This route returns all posts that contain the entered argument",
             tags = {"posts", "get"})
     @GetMapping("/title/{title}")
-    public ResponseEntity<List<Postagem>> getByTitle(@PathVariable String title){
+    public ResponseEntity<List<Postagem>> getByTitle(@PathVariable String title) {
         return ResponseEntity.ok(postagemRepository.findAllByTitleContainingIgnoreCase(title));
     }
 
@@ -57,19 +60,29 @@ public class PostagemController {
             description = "This route creates a new post",
             tags = {"post"})
     @PostMapping
-    public ResponseEntity<Postagem> create(@Valid @RequestBody Postagem postagem){
+    public ResponseEntity<Postagem> create(@Valid @RequestBody Postagem postagem) {
         if (temaRepository.existsById(postagem.getTema().getId()))
             return ResponseEntity.status(HttpStatus.CREATED)
-                .body(postagemRepository.save(postagem));
+                    .body(postagemRepository.save(postagem));
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não encontrado", null);
+    }
+
+    @PostMapping("/ia")
+    public ResponseEntity<String> generatePostByIa(@RequestBody String title, String theme) {
+        String prompt = String.format("Escreva um texto para um blog tecnológico com temática de detetive sobre %s focando em %s como assunto geral", theme, title);
+        if (!title.isEmpty() && !theme.isEmpty())
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(chatLanguageModel.generate(prompt));
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erro ao gerar postagem", null);
+
     }
 
     @Operation(summary = "Update post",
             description = "This route update a specific post based on the id",
             tags = {"update"})
     @PutMapping("/{id}")
-    public ResponseEntity<Postagem> update (@PathVariable UUID id, @Valid @RequestBody Postagem postagem){
-        if (postagemRepository.existsById(id)){
+    public ResponseEntity<Postagem> update(@PathVariable UUID id, @Valid @RequestBody Postagem postagem) {
+        if (postagemRepository.existsById(id)) {
             postagem.setId(id);
             if (temaRepository.existsById(postagem.getTema().getId()))
                 return ResponseEntity.status(HttpStatus.OK)
@@ -84,11 +97,11 @@ public class PostagemController {
             description = "This route deletes a specific post based on the id",
             tags = {"delete"})
     @DeleteMapping("/{id}")
-    public void delete (@PathVariable UUID id){
+    public void delete(@PathVariable UUID id) {
         Optional<Postagem> postagem = postagemRepository.findById(id);
 
-        if (postagem.isEmpty()){
-            throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (postagem.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
         postagemRepository.deleteById(id);
